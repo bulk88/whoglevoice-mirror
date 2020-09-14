d92e717508c77fcfbb10d37798c7032ec1b9c373
bulk88 <bulk88@hotmail.com>
2020-09-14 12:53:03 -0400

    Better Info on how long Creds are valid after fetch, not always 60 mins

--- getCredFull.js
+++ getCredFull.js
@@ -149,7 +149,7 @@ window.gapi.auth2.authorize({
             //copy can only be fired from a onclick event, so temp wipe GV interface, and put up a button
              var oldBodyNode = document.documentElement.removeChild(document.documentElement.getElementsByTagName('body')[0]);
              var newBodyNode = document.documentElement.appendChild(document.createElement('body'));
-             newBodyNode.appendChild(document.createTextNode("Got Account: "+resp.profile.email));
+             newBodyNode.appendChild(document.createTextNode("Got Account: "+resp.profile.email+' Exp In: '+(resp.expires_in/60)|0+':'+(resp.expires_in%60)+' Min'));
              newBodyNode.appendChild(document.createElement('br'));
              button_iframeNode = newBodyNode.appendChild(document.createElement('button'));
              button_iframeNode.innerText = "Click to Copy GV Auth Data";
